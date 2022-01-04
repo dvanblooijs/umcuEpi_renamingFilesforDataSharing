36a3230d8fa268c50c5e0b9cc71a3c0154feab29
Eline Schaft <eline@blue.umcutrecht.nl>
2022-01-04 17:28:56 +0100

    small error with merging

--- functions/renameFileVar.m
+++ functions/renameFileVar.m
@@ -1,72 +1,63 @@
-<<<<<<< HEAD
-function renameFileVar(filename,indivkey,renamekey)
-% author: Dorien van Blooijs
-% May 2021
-=======
 function renameFileVar(filename,indivkey,renamekey,checkTRC)
 % author: Dorien van Blooijs, Eline  Schaft
 % December 2021
->>>>>>> 39b4907 (Added check for annonimizing TRC files)
 
 [~,~, nameExt] = fileparts(filename);
 
 % if it is a .tsv-file
 if strcmp(nameExt,'.tsv')
-    
+
     % load file
     Variable = readtable(filename,'FileType','text','Delimiter','\t');
-    
+
     % find all column names
     allFields = fieldnames(Variable);
-    
+
     % check if any variable in each column contains the name that should be
     % replaced
     for i = 1:size(Variable,2)
-        
+
         if iscellstr(Variable.(allFields{i})) %#ok<ISCLSTR>
-        
+
             idx = contains(Variable.(allFields{i}),indivkey);
             Variable.(allFields{i})(idx) = replace(Variable.(allFields{i})(idx),indivkey,renamekey);
-            
+
         end
     end
 
-    % save file 
+    % save file
     writetable(Variable, filename, 'Delimiter', 'tab', 'FileType', 'text');
-    
+
 elseif strcmp(nameExt,'.mat')
-    
+
     % load file
     Variable = load(filename);
-    
-    % find all fields 
+
+    % find all fields
     allFields = fieldnames(Variable);
-    
+
     % check if any field contains the name that should be replaced
     for i = 1:size(allFields,1)
         if  isstring(Variable.(allFields{i})) || ischar(Variable.(allFields{i}))
-            
+
             Variable.(allFields{i}) = replace(Variable.(allFields{i}),indivkey,renamekey);
-            
+
         elseif iscellstr(Variable.(allFields{i}))
             idx = contains(Variable.(allFields{i}),indivkey);
             Variable.(allFields{i})(idx) = replace(Variable.(allFields{i})(idx),indivkey,renamekey);
         end
     end
-    
+
     % save file
     save(filename,'-struct','Variable')
-    
-<<<<<<< HEAD
-=======
+
 elseif strcmp(nameExt,'.TRC') && checkTRC
-    
-    
+
     % load TRC
     [fid,~] = fopen(filename,'r+');
 
     % Find the original names
-    fseek(fid,64,-1); subj_surname1   = char(fread(fid,22,'char'))'; 
+    fseek(fid,64,-1); subj_surname1   = char(fread(fid,22,'char'))';
     fseek(fid,86,-1); subj_name1   = char(fread(fid,20,'char'))';
 
     % Check whether subj_surname1 and subj_name1
@@ -84,7 +75,6 @@ elseif strcmp(nameExt,'.TRC') && checkTRC
     % Close the file (it will automatically be saved)
     fclose(fid);
 
->>>>>>> 39b4907 (Added check for annonimizing TRC files)
 else
     warning('Variables in %s have not been renamed, since it is not added yet',nameExt)
 end
