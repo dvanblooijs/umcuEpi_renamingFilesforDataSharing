7ae7e617dc614e32b23916a624bb91e93f25232c
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2022-05-05 13:09:31 +0200

    updated code for simplification

--- functions/renameFileContent.m
+++ functions/renameFileContent.m
@@ -1,8 +1,9 @@
 function renameFileContent(filename,key)
 % this script checks the content of a more general file, like
-% participants.tsv. This file does not contain a specific patient name in
-% the filename, but might contain several names in the content of the file.
-% This function removes original naming and replaces it with the naming
+% participants.tsv, dataset_description.json, scans.json etc. This file
+% does not contain a specific patient name in the filename, but might
+% contain several names in the content of the file. This function removes
+% original naming and replaces it with the naming
 % that is used for sharing data.
 
 % author: Dorien van Blooijs
@@ -18,25 +19,34 @@ if strcmp(nameExt,'.tsv')
     % load file
     Variable = readtable(filename,'FileType','text','Delimiter','\t');
 
-    % find all column names
-    allFields = fieldnames(Variable);
+    % find all column names (field names)
+    allFields = Variable.Properties.VariableNames;
 
-    % check if any variable in each column contains the name that should be
-    % replaced
-    for i = 1:size(Variable,2)
+    % VARIABLE NAMES: check if any fieldname contains a name that should be renamed
+    for jj = 1:size(allFields,2)
+        for ii = 1:size(origkey,1)
+            if contains(origkey{ii},allFields{jj})
+                allFields{jj} = replace(allFields{jj},origkey{ii},renamekey{ii});
+                Variable.Properties.VariableNames{jj} = allFields{jj};
+            end
+        end
+    end
 
-        if iscellstr(Variable.(allFields{i})) %#ok<ISCLSTR>            
-            for j = 1:size(Variable,1)
-                if contains(Variable.(allFields{i}){j},origkey)
-                    num = find(contains(origkey,Variable.(allFields{i}){j}) ==1);
-                    Variable.(allFields{i}){j}= replace(Variable.(allFields{i}){j},origkey(num),renamekey(num));
+    % VARIABLE CONTENT: check if any variable in each column contains a name that should be
+    % renamed
+    for ii = 1:size(Variable,2)
+
+        if iscellstr(Variable.(allFields{ii})) %#ok<ISCLSTR>            
+            for jj = 1:size(Variable,1)
+                if contains(Variable.(allFields{ii}){jj},origkey)
+                    num = find(contains(origkey,Variable.(allFields{ii}){jj}) ==1);
+                    Variable.(allFields{ii}){jj}= replace(Variable.(allFields{ii}){jj},origkey(num),renamekey(num));
                 end
             end
         end
     end
 
     Variable = bids_tsv_nan2na(Variable);
-
 
     % save file
     writetable(Variable, filename, 'Delimiter', 'tab', 'FileType', 'text');
@@ -46,18 +56,19 @@ elseif strcmp(nameExt,'.json')
     % load file
     Variable = read_json(filename);
 
-    for j=1:size(origkey,1)
-        indivkey = origkey{j}; renamekeyindiv = renamekey{j};
+    % for each key, check whether it is present in this specific json-file
+    for jj = 1:size(origkey,1)
+        indivkey = origkey{jj}; renamekeyindiv = renamekey{jj};
+        
+        % check both variable names and variable content
         Variable = renameStruct(Variable,indivkey,renamekeyindiv);
     end
     
     % save file
-    write_json(filename, newVariable);
+    write_json(filename, Variable);
+
 else
     warning('Variables in %s have not been renamed, since it is not added yet to function renameFileVar',nameExt)
 end
-
-
-
 
 end
