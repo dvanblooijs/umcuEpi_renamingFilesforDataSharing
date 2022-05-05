6396ff24330cc13ecc1661bd55bcb5c603798aff
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2022-05-05 13:08:28 +0200

    updated code when there is a datetime

--- external/bids_tsv_nan2na.m
+++ external/bids_tsv_nan2na.m
@@ -41,9 +41,13 @@ for kk = 1:length(var_names)
         thisColumn = num2cell(thisColumn); % make it a cell array
         thisColumn(isnan(t.(var_names{kk}))) = {'n/a'}; % replace NaN with 'n/a'
     elseif isdatetime(thisColumn)
-        thisColumn = {datestr(thisColumn)};
-%         thisColumn = num2cell(thisColumn);
-%         thisColumn(isnat(t.(var_names{kk}))) = {'n/a'}; % replace NaT with 'n/a'
+        thisColumn = datestr(thisColumn);
+        thisColumn_cell = cell(size(thisColumn,1),1);
+
+        for ll = 1:size(thisColumn,1)
+            thisColumn_cell{ll} = thisColumn(ll,:);
+        end
+
     else
         disp('column is not a cell, vector or datetime') % not sure whether this is possible, check anyways for sensible debug
     end
